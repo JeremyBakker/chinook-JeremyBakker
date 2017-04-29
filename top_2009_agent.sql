a36dd746625ed8d8a2d9d2ca31b26746d63c7ee6
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-04-28 19:12:04 -0500

    finished top_2009_agent.sql

---
+++ top_2009_agent.sql
@@ -0,0 +1,10 @@
+SELECT aggregateData.LastName "Sales Agent Last Name", aggregateData.FirstName "Sales Agent First Name", 
+    Max(aggregateData.sumTotal) "2009 Total Sales"
+FROM (SELECT e.LastName as LastName, e.FirstName as FirstName, c.CustomerId, SUM(i.Total) as sumTotal
+FROM Employee e
+INNER JOIN Customer c
+ON e.EmployeeId = c.SupportRepId
+LEFT JOIN Invoice i
+ON c.CustomerId = i.CustomerId
+WHERE i.InvoiceDate LIKE '%2009%'
+GROUP BY e.LastName) as aggregateData
